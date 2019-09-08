ea7fcf9f4c8cdd8e91e04256d37a4ec6ec440414
Aizadik <aizadaboposhova@gmail.com>
2019-09-08 09:04:33 -0500

    adding dependencies, created express server and innitialized port

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
 var path = require("path");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port that we are going to use in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing ???????
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
